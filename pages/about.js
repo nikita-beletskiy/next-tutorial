c23052ceb4fbf27a970fc4369f016c336819b723
Nikita Beletskiy <bel.nikita3197@gmail.com>
2021-04-15 09:51:42 +0300

    Initial commit
    Routing is done by creating page files in 'pages' folder. Every such file is basically just a React component. There is a Head tag where you define some custom props for head in your html.

---
+++ pages/about.js
@@ -0,0 +1,16 @@
+import Head from 'next/head';
+
+const About = () => {
+  return (
+    <div>
+      <Head>
+        <title>About title</title>
+        <meta name='keywords' content='about page' />
+      </Head>
+
+      <h1>About</h1>
+    </div>
+  );
+};
+
+export default About;
---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Home title</title>
+        <meta name='keywords' content='home page, initial page' />
+      </Head>
+
+      <h1>Welcome to Next</h1>
+    </div>
+  );
+}
